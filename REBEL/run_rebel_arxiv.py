30e45d25a8c6a6f89d5a8f99349c11e241f33414
eestien <stefan.pushkov@gmail.com>
2022-06-20 09:54:47 +0000

    Add kg analysis

--- REBEL/run_rebel_arxiv.py
+++ REBEL/run_rebel_arxiv.py
@@ -6,9 +6,13 @@ import time
 import re
 from tqdm import tqdm
 from text_preprocessing import *
+from nltk.stem import 	WordNetLemmatizer
 # import dask.bag as db
+import nltk
+from numpy.random import default_rng
 
-nlp_stem = spacy.load("en_core_web_sm")
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 nlp = spacy.load("en_core_web_lg")
 print("S")
@@ -22,13 +26,29 @@ papers = pd.read_pickle('../data/Arxiv/arxiv_papers_processed.pickle')
 out = {}
 start_time = time.time()
 
-for idx, input_text in tqdm(enumerate(papers['abstract'].values), total=len(papers['abstract'].values)):
+
+wordnet_lemmatizer = WordNetLemmatizer()
+
+rng = default_rng(seed=42)
+random_articles = rng.choice(len(papers['abstract'].values), size=250000, replace=False)
+
+
+for idx in tqdm(random_articles, total=len(random_articles)):
     try:
+        input_text = papers.iloc[idx, 1]
+        input_text = process_sent(input_text)
+        input_text = input_text.split('.')
         
-        input_text = process_sent(input_text)                 
-        # print(input_sentence)
-        doc = nlp_stem(input_text)
-        doc = nlp(" ".join([token.lemma_ for token in doc]))
+        text = ''
+        for i in input_text:
+            if i !='':
+                sent = ''
+                for j in i.split(' '):
+                    sent+= f'{wordnet_lemmatizer.lemmatize(j)} '
+                text+=f"{sent}."
+        input_text = text
+        doc = nlp(input_text)
+        
 
         for value, rel_dict in doc._.rel.items():
             out[f"{input_text}->{value}"] = {'relation': rel_dict['relation'], 'head': str(rel_dict['head_span']), 'tail': str(rel_dict['tail_span'])}
@@ -37,5 +57,5 @@ for idx, input_text in tqdm(enumerate(papers['abstract'].values), total=len(pape
         pass
 exec_time = time.time() - start_time
 out['time'] = exec_time
-with open('arxiv_rel_extracted.json', 'w') as fp:
+with open('arxiv_rel_extracted_250K.json', 'w') as fp:
     json.dump(out, fp)
